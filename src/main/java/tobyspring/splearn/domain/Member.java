0362d573743f3be7418efa77b3d913cd973f39b8
Toby Lee <tobyilee@gmail.com>
2025-05-28 06:58:08 +1000

    도메인 엔티티에서 JPA 매핑 정보 분리

--- src/main/java/tobyspring/splearn/domain/Member.java
+++ src/main/java/tobyspring/splearn/domain/Member.java
@@ -1,12 +1,11 @@
 package tobyspring.splearn.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.hibernate.annotations.NaturalId;
-import org.hibernate.annotations.NaturalIdCache;
 
 import static java.util.Objects.requireNonNull;
 import static org.springframework.util.Assert.state;
@@ -15,12 +14,7 @@ import static org.springframework.util.Assert.state;
 @Getter
 @ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-@NaturalIdCache
-public class Member {
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-
-    @Embedded
+public class Member extends AbstractEntity {
     @NaturalId
     private Email email;
 
@@ -28,10 +22,9 @@ public class Member {
 
     private String passwordHash;
 
-    @Enumerated(EnumType.STRING)
     private MemberStatus status;
 
-    public static Member register(MemberRegisterRequest createRequest,  PasswordEncoder passwordEncoder) {
+    public static Member register(MemberRegisterRequest createRequest, PasswordEncoder passwordEncoder) {
         Member member = new Member();
 
         member.email = new Email(createRequest.email());
--- src/test/java/tobyspring/splearn/adapter/integration/DummyEmailSenderTest.java
+++ src/test/java/tobyspring/splearn/adapter/integration/DummyEmailSenderTest.java
@@ -6,7 +6,6 @@ import org.junitpioneer.jupiter.StdOut;
 import tobyspring.splearn.domain.Email;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class DummyEmailSenderTest {
     @Test
